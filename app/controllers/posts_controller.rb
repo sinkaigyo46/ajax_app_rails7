b38d0a8e5fa2e40fb5574ae4a8c0ec0dfedd7d5d
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-07-20 21:34:05 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,11 @@
+
 class PostsController < ApplicationController
   def index
     @posts = Post.order(id: "DESC")
   end
-
-  def new
-  end
-
+ 
   def create
-    post = Post.create(content: params[:content])
-    render json: (post: post)
+    Post.create(content: params[:content])
+    redirect_to action: :index
   end
-end
+ end
